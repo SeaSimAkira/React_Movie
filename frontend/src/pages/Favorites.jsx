9602b90566869b097aa0078bff599d14920f85bd
Mac <mac@Macs-MacBook-Pro.local>
2026-05-08 10:08:29 +0700

    SeaSim React Part1

---
+++ frontend/src/pages/Favorites.jsx
@@ -0,0 +1,8 @@
+import "../css/Favorites.css"
+function Favorites() {
+  return <div className="favorites-empty">
+    <h2>No Favorite Movies Yet</h2>
+    <p>Strat adding movies to your favorite and they will appear hear</p>
+  </div>
+}
+export default Favorites
